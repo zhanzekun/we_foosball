437ed5f8e1d4fe85bdd89a4373123247b5fb2714
zekunzhan <zekunzhan@tencent.com>
2025-07-01 05:27:39 +0800

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -88,12 +88,18 @@ export default function WelcomePage() {
           <div className="welcome-title">WeFoosball</div>
         </div>
         <div className="welcome-subtitle">WXG 桌上足球协会</div>
-      <div className="welcome-version">构建号: {process.env.NEXT_PUBLIC_BUILD_ID}</div>
+        <div className="welcome-version">构建号: {process.env.NEXT_PUBLIC_BUILD_ID}</div>
       </div>
       <div className="welcome-login-bottom">
         <div className="welcome-login-row">
           <button className="login-btn google" onClick={handleGoogleLogin}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" style={{ marginRight: 8 }}><g><path d="M21.805 10.023h-9.765v3.977h5.617c-.242 1.242-1.484 3.648-5.617 3.648-3.383 0-6.148-2.797-6.148-6.25s2.765-6.25 6.148-6.25c1.93 0 3.227.82 3.969 1.523l2.711-2.633c-1.711-1.57-3.922-2.523-6.68-2.523-5.523 0-10 4.477-10 10s4.477 10 10 10c5.742 0 9.547-4.031 9.547-9.719 0-.652-.07-1.148-.156-1.523z" fill="#4285F4" /><path d="M3.152 7.345l3.281 2.406c.891-1.781 2.578-2.906 4.617-2.906 1.102 0 2.148.383 2.953 1.016l2.953-2.883c-1.602-1.477-3.672-2.375-5.906-2.375-3.672 0-6.773 2.477-7.891 5.812z" fill="#34A853" /><path d="M12 22c2.672 0 4.922-.883 6.563-2.406l-3.047-2.492c-.828.578-1.891.922-3.516.922-2.828 0-5.227-1.906-6.086-4.453l-3.242 2.5c1.672 3.406 5.25 5.929 9.328 5.929z" fill="#4CAF50" /><path d="M21.805 10.023h-9.765v3.977h5.617c-.242 1.242-1.484 3.648-5.617 3.648-3.383 0-6.148-2.797-6.148-6.25s2.765-6.25 6.148-6.25c1.93 0 3.227.82 3.969 1.523l2.711-2.633c-1.711-1.57-3.922-2.523-6.68-2.523-5.523 0-10 4.477-10 10s4.477 10 10 10c5.742 0 9.547-4.031 9.547-9.719 0-.652-.07-1.148-.156-1.523z" fill="#4285F4" fillOpacity=".1" /></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20" style={{ marginRight: 8 }}>
+              <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
+              <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
+              <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
+              <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
+              <path d="M1 1h22v22H1z" fill="none" />
+            </svg>
             Google
           </button>
           <button className="login-btn github" onClick={handleGithubLogin}>
